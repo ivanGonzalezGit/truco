998d580898ecd5546d81642bbe9b9f98bd846afb
Iván González <ivanfgonzalezruiz@gmail.com>
2025-03-15 10:00:50 -0300

    pantalla de terminal

--- graficos.c
+++ graficos.c
@@ -4,16 +4,22 @@
 #define ALTO_MATRIZ 24
 #define ANCHO_CARTA 13
 #define ALTO_CARTA 8
+#define POS_1_ANCHO 2
+#define POS_1_ALTO 2
 
 void dorsoCarta(int carta[ALTO_CARTA][ANCHO_CARTA]);
-void pantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ]);
+
+void pantalla(  int panio[ALTO_MATRIZ][ANCHO_MATRIZ], 
+                int carta[ALTO_CARTA][ANCHO_CARTA]);
+
 void imprimirPantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ]);
 
 int main()
 {
     int panio[ALTO_MATRIZ][ANCHO_MATRIZ];
+    int carta[ALTO_CARTA][ANCHO_CARTA];
 
-    pantalla(panio);
+    pantalla(panio, carta);
     imprimirPantalla(panio);
     
     return 0;
@@ -46,7 +52,7 @@ void dorsoCarta(int carta[ALTO_CARTA][ANCHO_CARTA])
     carta[ALTO_CARTA - 1][ANCHO_CARTA - 1] = 217;
 }
 
-void pantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ])
+void pantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ], int carta[ALTO_CARTA][ANCHO_CARTA])
 {
     for (int i = 0; i < ALTO_MATRIZ; i++)
     {
@@ -62,7 +68,7 @@ void pantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ])
             }
             else
             {
-                panio[i][j] = 176;
+                panio[i][j] = 32;
             } 
         }
     }
@@ -71,6 +77,30 @@ void pantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ])
     panio[0][ANCHO_MATRIZ - 1] = 187;
     panio[ALTO_MATRIZ - 1][0] = 200;
     panio[ALTO_MATRIZ - 1][ANCHO_MATRIZ - 1] = 188;
+
+    for (int i = 0+POS_1_ANCHO; i < ALTO_CARTA+POS_1_ANCHO; i++)
+    {
+        for (int j = 0+POS_1_ALTO; j < ANCHO_CARTA+POS_1_ALTO; j++)
+        {
+            if (j == 0 || j == ANCHO_CARTA - 1)
+            {
+                panio[i][j] = 179;
+            }
+            else if (i == 0 || i == ALTO_CARTA - 1)
+            {
+                panio[i][j] = 196;            
+            }
+            else
+            {
+                panio[i][j] = 177;
+            } 
+        }
+    }
+
+    carta[0+POS_1_ALTO][0+POS_1_ANCHO] = 218;
+    carta[0+POS_1_ALTO][ANCHO_CARTA - 1+POS_1_ANCHO] = 191;
+    carta[ALTO_CARTA - 1+POS_1_ALTO][0+POS_1_ANCHO] = 192;
+    carta[ALTO_CARTA - 1+POS_1_ALTO][ANCHO_CARTA - 1+POS_1_ANCHO] = 217;
 }
 
 void imprimirPantalla(int panio[ALTO_MATRIZ][ANCHO_MATRIZ])
